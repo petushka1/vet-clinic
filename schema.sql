83497db699721786397505402a46df117930221b
petushka1 <nadya@devery.io>
2022-08-27 18:25:20 +0300

    add milestone 4

--- schema.sql
+++ schema.sql
@@ -37,3 +37,31 @@ CREATE TABLE species (
     name VARCHAR(100), 
     PRIMARY KEY(id)
     );
+
+/* Milestone-4 */
+    CREATE TABLE vets (
+        id  INT GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(100),
+        age INT,
+        date_og_graduation  DATE NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+    CREATE TABLE specializations (
+        species_id INT,
+        vets_id INT,
+        PRIMARY KEY (species_id, vets_id),
+        FOREIGN KEY (vetS_id) REFERENCES vets (id) ON DELETE CASCADE,
+        FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE
+    );
+
+  CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    visitation_date DATE NOT NULL,
+    PRIMARY KEY (animals_id, vets_id, visitation_date),
+    FOREIGN KEY (vets_id) REFERENCES vets (id) ON DELETE CASCADE,
+    FOREIGN KEY (animals_id) REFERENCES animals (id) ON DELETE CASCADE
+);
+
+
